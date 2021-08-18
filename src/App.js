0cfca484677c88222ede8eeacdc73ffb1ec767a4
LeoSandrini <thesandrini@gmail.com>
2021-08-17 23:41:15 -0300

    Setting exchange rate options as values for input fields

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CurrencyRow from './CurrencyRow';
 import './App.css';
 import axios from 'axios';
 
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-
-// app.use(
-//   cors({
-//     origin: 'https://swapi.dev/api/'
-//   })
-// );
-
-const API_KEY = 'a2a7cdfcc97ac77ffb9a523ce4d10113';
-const BASE_URL = 'https://data.fixer.io/api/latest';
-const URL = BASE_URL + '?access_key=' + API_KEY;
+const API_KEY = '716b4e3f1c4e666c7941e800';
+const BASE_URL = 'https://v6.exchangerate-api.com/v6/';
+const URL = BASE_URL + +API_KEY + '/latest/USD';
 
 export default function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
   useEffect(async () => {
     const resp = await axios.get(
       `https://v6.exchangerate-api.com/v6/716b4e3f1c4e666c7941e800/latest/USD`
     );
-    console.log(resp.data);
+    setCurrencyOptions([
+      // resp.data.base_code,
+      ...Object.keys(resp.data.conversion_rates)
+    ]);
   }, []);
 
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
     </>
   );
 }
